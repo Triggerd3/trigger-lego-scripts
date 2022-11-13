086a13ecb7fd61635deeaf8870d5ca6666829d19
Triggerd3 <88150311+Triggerd3@users.noreply.github.com>
2022-11-13 14:57:05 +0100

    Add files via upload

---
+++ shocked-emojo.lua
@@ -0,0 +1,20 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Triggerd3/trigger-lego-scripts/main/edited-hub.lua", true))()
+
+local GameTitle = ""
+local GameList = {
+	["Fakewoken 3"] = 8350658333
+}
+for Name,ID in next, GameList do
+	if game.GameId == ID then
+		GameTitle = Name
+	elseif game.PlaceId == ID then
+		GameTitle = Name
+	end
+end
+
+if GameTitle == "Fakewoken 3" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Triggerd3/trigger-lego-scripts/main/Games/squidward-emoji.lua"))
+end
+
+
+library:Init()
